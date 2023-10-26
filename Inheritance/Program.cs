752aff2e79337b2a34d23bd8c265807801088162
serkan <148799200+serkanozak@users.noreply.github.com>
2023-10-26 17:48:28 +0300

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName = "Engin"}, new Student{FirstName = "Derin"}, new Person{FirstName = "Salih"}
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Person2
+    {
+
+    }
+
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+}
